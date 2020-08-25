7f8fca9611061647039a153309ee2a7040ef8f23
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-08-25 13:16:38 +0530

    version 1 pageblock setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,4 @@
-const config = require("./config.json")
-const infoData = require("./content/data/info.json")
-
 module.exports = {
-  //this makes the site config available to forestry cms
-  siteMetadata: {
-    title: config.title,
-    description: config.description,
-    repoUrl: config.repository_url,
-    about: config.about,
-    contact: config.contact,
-    primaryColor: config.primary_color,
-    infoData: infoData
-  },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-transformer-remark",
@@ -25,20 +12,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "posts",
-        path: `${__dirname}/content/posts`
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "data",
-        path: `${__dirname}/content/data`,
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: "images",
@@ -46,10 +19,17 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-sharp", 
+      resolve: `gatsby-source-filesystem`,
       options: {
-        defaultQuality: 75
-      }
+        name: "pages",
+        path: `${__dirname}/content/pages`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-sharp",
+      options: {
+        defaultQuality: 75,
+      },
     },
     `gatsby-transformer-sharp`,
     {
