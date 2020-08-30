d2bcda871e6a9b39c70491bfda541f836f1cd140
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-08-30 13:59:50 +0530

    Add cta component

---
+++ src/components/Cta.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { Text, Heading } from "@chakra-ui/core"
+
+function Cta({ props }) {
+  console.log(props)
+  var compProps = {}
+  props.props.map((el, i) => {
+    var prop = el.split("=")
+    console.log(prop)
+    var temp
+    if (prop[1].includes("|")) temp = prop[1].split("|")
+    console.log(temp)
+    const value = prop[1].includes("|") ? temp : prop[1]
+    compProps = {
+      ...compProps,
+      [prop[0]]: value,
+    }
+    console.log(compProps)
+  })
+
+  return (
+    <>
+      {props.type === "Title" && (
+        <Heading as="h1" {...compProps}>
+          {props.content}
+        </Heading>
+      )}
+      {props.type === "Subtitle" && (
+        <Heading as="h2" {...compProps}>
+          {props.content}
+        </Heading>
+      )}
+      {props.type === "Description" && (
+        <Text as="p" {...compProps}>
+          {props.content}
+        </Text>
+      )}
+    </>
+  )
+}
+
+export default Cta
